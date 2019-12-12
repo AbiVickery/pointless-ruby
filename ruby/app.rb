6343c9d5d45de3179cc7907654453f10bad83e8c
Abi Vickery <vickeryabi@gmail.com>
2019-12-12 16:39:12 -0600

    echo and cat

--- ruby/app.rb
+++ ruby/app.rb
@@ -16,3 +16,11 @@ pineapple
 $ grep toes list
 potatoes
 tomatoes
+
+# Im honestly just typing in random commands in ruby until I get around to actually learning Ruby
+# When I do get around to learning Ruby, I might just learn Ruby and SASS (scss) together
+
+$ echo pizza > list
+
+$ cat list
+pizza
